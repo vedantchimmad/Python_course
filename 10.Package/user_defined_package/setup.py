f3bba35c34af63491437bb3cbb6b5d9b90693ab0
Vedant249 <vedanchimmad@gmail.com>
2024-02-04 23:14:43 +0530

    Adding setup files

--- 10.Package/user_defined_package/setup.py
+++ 10.Package/user_defined_package/setup.py
@@ -6,9 +6,8 @@ setup(
     description='A sample Python package',
     author='vedant',
     author_email='vedantchimmad@gmail.com',
-    packages=['my_package'],
     install_requires=[
         'numpy',
         'pandas',
     ],
-) 
+)
